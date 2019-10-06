2d3e54a52e28c01f55f488ea02d831c040fa8bb8
amrish_kulasekaran <amrish_kulasekaran@dynattral.com>
2019-10-06 02:55:11 +0100

    completed - initially - EV  is 143178.762

--- linearRegression.py
+++ linearRegression.py
@@ -4,7 +4,8 @@ import pandas as pd
 import numpy
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelBinarizer
- 
+from sklearn.metrics import mean_squared_error, r2_score
+
 # Dataset Path
 testDS_path = "tcd ml 2019-20 income prediction test (without labels).csv"
 trainDS_path = "tcd ml 2019-20 income prediction training (with labels).csv"
@@ -71,10 +72,6 @@ def FeatureExtraction(dataFrame):
 
     return dataFrame
 
-def ModelCreation(xFrame, yFrame):
-    return LinearRegression().fit(xFrame,yFrame)
-
-
 def Preprocessing(dataFrame):
     
     #Managing nulls
@@ -86,28 +83,97 @@ def Preprocessing(dataFrame):
     #feature extraction
     dataFrame = FeatureExtraction(dataFrame)
 
+    #Initial attempt drop -> Year of record, country and city size
+    dataFrame = dataFrame.drop(['Year of Record'], axis = 1)
+    dataFrame = dataFrame.drop(['Country'], axis = 1)
+    dataFrame = dataFrame.drop(['Size of City'], axis = 1)
+    dataFrame = dataFrame.drop(['Body Height [cm]'], axis = 1)
+
     return dataFrame
 
-def run():
-
+def PreprocessingTrainingDS():
     #load data
     trainingFrame = pd.read_csv(trainDS_path)
 
-    #preprocessing
-    processedTrainingFrame = Preprocessing(trainingFrame) 
+    #preprocessing - basic
+    processedTrainingFrame = Preprocessing(trainingFrame)
 
-    #create model and train
-    #linearModel = ModelCreation(processedTrainingFrame,trainingFrame['Income'])
+    #get the list of profession
+    trainingProfessionList = processedTrainingFrame['Profession'].unique()
+    
+    #create a label binary format
+    lbFormat = LabelBinarizer(neg_label=0, pos_label=1, sparse_output=False)
 
+    #Extract the profession column and join the data frame
+    processedTrainingFrame = processedTrainingFrame.join(pd.DataFrame(lbFormat.fit_transform(processedTrainingFrame['Profession']),columns=lbFormat.classes_,index=processedTrainingFrame.index))
+
+    #Add a new column -> Other Profession
+    dummyData = [0] * len(processedTrainingFrame['Profession'])
+    processedTrainingFrame['Other Profession'] = dummyData
+
+    #Remove the profession Column
+    processedTrainingFrame = processedTrainingFrame.drop(['Profession'], axis = 1)
+
+    return processedTrainingFrame.drop(['Income in EUR'],axis = 1), processedTrainingFrame['Income in EUR']
+
+
+def PreprocessingTestDS():
     testFrame = pd.read_csv(testDS_path)
+
+    #Remove income Column
+    testFrame = testFrame.drop(['Income'],axis = 1)
+
     processedTestFrame = Preprocessing(testFrame)
 
-    print(processedTrainingFrame)
-    print(processedTestFrame)
+    #Formate the profession column
+    trainingFrame = pd.read_csv(trainDS_path)
+    refProfessionList = list(set(processedTestFrame['Profession'].unique()) - set(trainingFrame['Profession'].unique()))
+    processedTestFrame['Profession'] = processedTestFrame['Profession'].replace(refProfessionList,'Other Profession')
 
+    
+    #create a label binary format
+    lbFormat = LabelBinarizer(neg_label=0, pos_label=1, sparse_output=False)
+
+    #Extract the profession column and join the data frame
+    processedTestFrame = processedTestFrame.join(pd.DataFrame(lbFormat.fit_transform(processedTestFrame['Profession']),columns=lbFormat.classes_,index=processedTestFrame.index))
+
+    #Add a new columns to match the training frame
+    dummyData = [0] * len(processedTestFrame['Profession'])
+    neededProfessionList = list(set(trainingFrame['Profession'].unique()) - set(processedTestFrame['Profession'].unique()))
+
+    for prof in neededProfessionList:
+        processedTestFrame[prof] = dummyData
+
+    #Remove the profession Column
+    processedTestFrame = processedTestFrame.drop(['Profession'], axis = 1)
+    return processedTestFrame
+
+def ModelCreation(xFrame, yFrame):
+    return LinearRegression().fit(xFrame,yFrame)
+
+def run():
+
+    print("stated training data preprocessing")
+    #load and preprocess training data
+    (xDataFrame,yDataFrame) = PreprocessingTrainingDS()
+    
+    print("stated creating linear model")
+    #create model and train
+    linearModel = ModelCreation(xDataFrame,yDataFrame)
+
+    print("stated training data preprocessing")
+    #load and preprocess test data
+    testDataFrame = PreprocessingTestDS()
+
+    print("stated prediction")   
     #prediction
-    #linearModel.predict(processedTestFrame)
+    prediction = linearModel.predict(testDataFrame)
+    
+    print("pusing to a file") 
+    numpy.savetxt('predictedOutput.csv',prediction)
 
+    print("Mean Square Error is " + str(linearModel.score(testDataFrame,prediction)))
+    
 
 if __name__ == '__main__':
     run()
